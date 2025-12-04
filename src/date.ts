4f29f4b6dc53b6dc0d628a01dfd98950f2d8dda5
maxgfr <25312957+maxgfr@users.noreply.github.com>
2025-12-04 22:31:28 +0100

    fix: ci npm publish

--- src/date.ts
+++ src/date.ts
@@ -3,7 +3,7 @@ export class RnDate extends Date {
     if (dateString === undefined || dateString === null) {
       super();
     } else {
-      let dateParam = dateString.split(/[\s-:]/);
+      const dateParam = dateString.split(/[\s-:]/);
       if (dateParam.length > 3) {
         dateParam[1] = (parseInt(dateParam[1], 10) - 1).toString();
         super(...(dateParam as []));
